c428bfdb0184e9340a36339b7e914178743154f5
kishiguro <ishi@hash-set.com>
2018-02-23 08:19:15 +0900

    VRF ID is changed from int to uint32.

--- rib/vrf.go
+++ rib/vrf.go
@@ -35,32 +35,42 @@ var (
 	VrfMutex sync.RWMutex
 )
 
+type WatcherRedist struct {
+	typ [RIB_MAX]Watchers
+	def Watchers
+}
+
 type Vrf struct {
 	Name        string
-	Index       int
+	Id          uint32
 	IfTable     *netutil.Ptree
 	IfMap       map[string]*Interface
 	ribTable    [AFI_MAX]*netutil.Ptree
 	nhopTable   [AFI_MAX]*netutil.Ptree
 	arpTable    [AFI_MAX]*netutil.Ptree
 	staticTable [AFI_MAX]*netutil.Ptree
+	redist      [AFI_MAX]WatcherRedist
 	routerId    RouterId
 	ZServer     *ZServer
 	Mutex       sync.RWMutex
 	IfMutex     sync.Mutex
-	Watcher     map[*IfWatcher]bool
+	Watcher     map[*IfWatcher]bool // Will be merged to Watchers
 	WMutex      sync.Mutex
+	Watchers    []Watchers
 }
+
+// For all VRF redistribute.
+var Redist [AFI_MAX]WatcherRedist
 
 func VrfDefault() *Vrf {
 	return VrfTable[0]
 }
 
-func VrfLookupByIndex(index int) *Vrf {
-	if index < 0 || index >= VRF_ID_MAX {
+func VrfLookupByIndex(id uint32) *Vrf {
+	if id < 0 || id >= VRF_ID_MAX {
 		return nil
 	}
-	return VrfTable[index]
+	return VrfTable[id]
 }
 
 func VrfLookupByName(name string) *Vrf {
@@ -70,33 +80,34 @@ func VrfLookupByName(name string) *Vrf {
 	return nil
 }
 
-func NewVrf(name string, index int) *Vrf {
-	v := &Vrf{
-		Name:    name,
-		Index:   index,
-		IfTable: netutil.NewPtree(32),
-		IfMap:   make(map[string]*Interface),
-		Watcher: make(map[*IfWatcher]bool),
+func NewVrf(name string, vrfId uint32) *Vrf {
+	vrf := &Vrf{
+		Name:     name,
+		Id:       vrfId,
+		IfTable:  netutil.NewPtree(32),
+		IfMap:    make(map[string]*Interface),
+		Watcher:  make(map[*IfWatcher]bool),
+		Watchers: make([]Watchers, WATCH_TYPE_MAX),
 	}
-	v.ribTable[AFI_IP] = netutil.NewPtree(32)
-	v.ribTable[AFI_IP6] = netutil.NewPtree(128)
-	v.nhopTable[AFI_IP] = netutil.NewPtree(32)
-	v.nhopTable[AFI_IP6] = netutil.NewPtree(128)
-	v.arpTable[AFI_IP] = netutil.NewPtree(32)
-	v.arpTable[AFI_IP6] = netutil.NewPtree(128)
-	v.staticTable[AFI_IP] = netutil.NewPtree(32)
-	v.staticTable[AFI_IP6] = netutil.NewPtree(128)
+	vrf.ribTable[AFI_IP] = netutil.NewPtree(32)
+	vrf.ribTable[AFI_IP6] = netutil.NewPtree(128)
+	vrf.nhopTable[AFI_IP] = netutil.NewPtree(32)
+	vrf.nhopTable[AFI_IP6] = netutil.NewPtree(128)
+	vrf.arpTable[AFI_IP] = netutil.NewPtree(32)
+	vrf.arpTable[AFI_IP6] = netutil.NewPtree(128)
+	vrf.staticTable[AFI_IP] = netutil.NewPtree(32)
+	vrf.staticTable[AFI_IP6] = netutil.NewPtree(128)
 
-	v.routerId.Init()
+	vrf.routerId.Init()
 
-	VrfTable[v.Index] = v
-	VrfMap[v.Name] = v
+	VrfTable[vrf.Id] = vrf
+	VrfMap[vrf.Name] = vrf
 
-	if index != 0 {
-		v.ZServer = ZServerStart("unix", fmt.Sprintf("/var/run/zserv-vrf%d.api", index), index)
+	if vrfId != 0 {
+		vrf.ZServer = ZServerStart("unix", fmt.Sprintf("/var/run/zserv-vrf%d.api", vrfId), vrfId)
 	}
 
-	return v
+	return vrf
 }
 
 func VrfDefaultZservStart() {
@@ -106,28 +117,28 @@ func VrfDefaultZservStart() {
 	}
 }
 
-func VrfAssignIndex() int {
+func VrfAssignIndex() uint32 {
 	for i := VRF_ID_MIN + 100; i <= VRF_ID_MAX; i++ {
 		if VrfTable[i] == nil {
-			return i
+			return uint32(i)
 		}
 	}
 	return 0
 }
 
-func VrfAssign(name string, index int) (*Vrf, error) {
-	if index == 0 {
-		index = VrfAssignIndex()
-		if index == 0 {
+func VrfAssign(name string, id uint32) (*Vrf, error) {
+	if id == 0 {
+		id = VrfAssignIndex()
+		if id == 0 {
 			return nil, fmt.Errorf("Can't assing VRF index")
 		}
 	}
-	v := NewVrf(name, index)
+	v := NewVrf(name, id)
 
 	return v, nil
 }
 
-func VrfExtractIndex(name string) int {
+func VrfExtractIndex(name string) uint32 {
 	r := regexp.MustCompile("vrf(\\d+)")
 	matches := r.FindAllStringSubmatch(name, -1)
 	if matches != nil && len(matches) > 0 {
@@ -135,7 +146,7 @@ func VrfExtractIndex(name string) int {
 		if len(match) >= 2 {
 			fmt.Println("VrfExtractIndex", match[1])
 			index, _ := strconv.Atoi(match[1])
-			return index
+			return uint32(index)
 		}
 	}
 	return 0
@@ -143,7 +154,7 @@ func VrfExtractIndex(name string) int {
 
 func VrfStop() {
 	for _, vrf := range VrfMap {
-		if vrf.Index != 0 {
+		if vrf.Id != 0 {
 			server.VrfDelete(vrf.Name)
 		}
 	}
